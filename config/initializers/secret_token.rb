d30365ec0d1a1f8616966fffc05525daaed00af5
Florence Foo <pandamouse@gmail.com>
2012-02-07 12:54:33 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittleGems::Application.config.secret_token = 'bae54714f2dcf84c31f27f84c3e0839cf9ced97be90a6472fe40af449d223f147f324fa6c39908700f99bf24c71fd7240db2b271335fb11c10bf5c362baa5c61'
